8329284d78b517c425ceb69ef2ad30f30878c82b
Majji23 <majjipraveen21@gmail.com>
2025-06-22 11:21:16 +0530

    Add files via upload
    PrimitiveDataTypes

---
+++ Week1Day1/VariableScoping.js
@@ -0,0 +1,12 @@
+const browserVersion= 'chrome'
+
+function getBrowserVersion(){
+    if(browserVersion==='chrome'){
+        var browserVersion= 'local chrome'
+       // let browserVersion = 'local chrome';
+    }
+    console.log("local Browser Version:", browserVersion)
+}
+getBrowserVersion()
+console.log("Browser Version:", browserVersion)
+
---
+++ Week1Day1/primitveDataTypes.js
@@ -0,0 +1,14 @@
+
+// Declare and assign variables
+let firstName = "Praveen";            // string
+let companyName = "Qsome Technologies"; // string
+let mobileNumber = 9876543210;        // number
+let isAutomation = true;              // boolean
+let hasPlaywright;                    // undefined (uninitialized)
+
+// Print values and their types
+console.log("firstName:", firstName, "| type:", typeof firstName);
+console.log("companyName:", companyName, "| type:", typeof companyName);
+console.log("mobileNumber:", mobileNumber, "| type:", typeof mobileNumber);
+console.log("isAutomation:", isAutomation, "| type:", typeof isAutomation);
+console.log("hasPlaywright:", hasPlaywright, "| type:", typeof hasPlaywright);
